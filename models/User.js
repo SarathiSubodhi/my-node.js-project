0221497c63b68ca2a7bf058c568e8d0e29826bf8
SarathiSubodhi <it21800450@my.sliit.lk>
2024-07-11 14:39:45 +0530

    Set Up the modeles and add data

---
+++ models/User.js
@@ -0,0 +1,27 @@
+
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  weatherData: [{
+    date: String,
+    data: Object,
+  }],
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
