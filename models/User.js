5f735b66f35464577596621c9668f3220aa9a8db
SarathiSubodhi <it21800450@my.sliit.lk>
2024-07-11 14:42:16 +0530

    add modeles and data

--- models/User.js
+++ models/User.js
@@ -1,1 +1,28 @@
 
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  weatherData: [{
+    date: String,
+    data: Object,
+  }],
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
+
