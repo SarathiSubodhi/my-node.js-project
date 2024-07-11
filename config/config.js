e34ccfe89ca120736e65cfd37de46df39382a906
SarathiSubodhi <it21800450@my.sliit.lk>
2024-07-11 12:28:25 +0530

    created the  environment variables

---
+++ config/config.js
@@ -0,0 +1,11 @@
+// config/config.js
+
+require('dotenv').config();
+
+module.exports = {
+  dbUri: process.env.DB_URI || 'mongodb://localhost:27017/mydb',
+  port: process.env.PORT || 3000,
+  emailUser: process.env.EMAIL_USER,
+  emailPass: process.env.EMAIL_PASS,
+  weatherApiKey: process.env.WEATHER_API_KEY,
+};
